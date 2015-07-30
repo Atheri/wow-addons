11ec73c610e41e9e128ccc1da7cb77e7d14a8cac
Cory Lotze <corylotze@gmail.com>
2015-07-30 13:59:27 -0700

    Converted tabs to spaces. Didn't realize notepad++ was using tabs until I saw the code on github

--- KirossShh/KirossShh.lua
+++ KirossShh/KirossShh.lua
@@ -1,15 +1,15 @@
 kiross_chars = {"Atheri"}
 
 chat_table = {
-	["CHAT_MSG_CHANNEL"] = "CHANNEL",
-	["CHAT_MSG_GUILD"] = "GUILD",
-	["CHAT_MSG_PARTY"] = "PARTY",
-	["CHAT_MSG_RAID"] = "RAID",
-	["CHAT_MSG_SAY"] = "SAY",
-	["CHAT_MSG_YELL"] = "YELL",
-	["CHAT_MSG_EMOTE"] = "EMOTE",
-	["CHAT_MSG_INSTANCE_CHAT"] = "INSTANCE_CHAT",
-	["CHAT_MSG_TEXT_EMOTE"] = "EMOTE"
+    ["CHAT_MSG_CHANNEL"] = "CHANNEL",
+    ["CHAT_MSG_GUILD"] = "GUILD",
+    ["CHAT_MSG_PARTY"] = "PARTY",
+    ["CHAT_MSG_RAID"] = "RAID",
+    ["CHAT_MSG_SAY"] = "SAY",
+    ["CHAT_MSG_YELL"] = "YELL",
+    ["CHAT_MSG_EMOTE"] = "EMOTE",
+    ["CHAT_MSG_INSTANCE_CHAT"] = "INSTANCE_CHAT",
+    ["CHAT_MSG_TEXT_EMOTE"] = "EMOTE"
 }
 
 local frame = CreateFrame("Frame")
@@ -27,27 +27,27 @@ KirossShh_disable = false
 
 frame:SetScript("OnEvent", 
 function(self, event, message, sender, _, _, _, _, _, chan_num, chan_name, ...)
-	-- Remove the realm name from sender name
-	local index = string.find(sender, "-")
-	name = sender
-	if index ~= nil then
-		name = string.sub(sender, 0, index-1)
-	end
-	
-	if name == kiross_chars[1] and not KirossShh_disable then
-		-- Message to send
-		local text
-	
-		if chat_table[event] == "EMOTE" then
-			DoEmote("SILENCE", name)
-		else
-			text = kiross_chars[1] .. " Shhh"
-	
-			SendChatMessage(text, chat_table[event], nil, chan_num)
-			print(event)
-			print(chan_num, chan_name, name, message)
-		end
-	end	
+    -- Remove the realm name from sender name
+    local index = string.find(sender, "-")
+    name = sender
+    if index ~= nil then
+        name = string.sub(sender, 0, index-1)
+    end
+    
+    if name == kiross_chars[1] and not KirossShh_disable then
+        -- Message to send
+        local text
+    
+        if chat_table[event] == "EMOTE" then
+            DoEmote("SILENCE", name)
+        else
+            text = kiross_chars[1] .. " Shhh"
+    
+            SendChatMessage(text, chat_table[event], nil, chan_num)
+            print(event)
+            print(chan_num, chan_name, name, message)
+        end
+    end 
 end)
 
 --[[
--- TelchMain/TelchMain.lua
+++ TelchMain/TelchMain.lua
@@ -1,35 +1,35 @@
 name_table = {
-			["Telch"] = "druid",
-			["Telchor"] = "shaman", 
-			["Dandelo"] = "mage", 
-			["Dandelock"] = "lock", 
-			["Heyoh"] = "death knight", 
-			["Flagg"] = "warrior",
-			["Manjello"] = "paladin"}
+            ["Telch"] = "druid",
+            ["Telchor"] = "shaman", 
+            ["Dandelo"] = "mage", 
+            ["Dandelock"] = "lock", 
+            ["Heyoh"] = "death knight", 
+            ["Flagg"] = "warrior",
+            ["Manjello"] = "paladin"}
 
 local LoginFrame = CreateFrame("Frame")
 LoginFrame:RegisterEvent("CHAT_MSG_SYSTEM")
 
 LoginFrame:SetScript("OnEvent", 
 function(self, event, ...)
-	local some_var = ...
-	local name = some_var:match( "^.*%[(.*)%].* has come online")
-	if name then
-		for char_name, class in pairs(name_table) do
-			if name == char_name then
-				if name == "Dandelo" then
-					local msg = "False alarm, telch's main is still " .. class
-					SendChatMessage(msg, "GUILD")
-					break
-				else
-					local msg = "Ohay, telch is on his " .. class .. " how's the main switch going?"
-					SendChatMessage(msg, "GUILD")
-				end
-			end
-		end
-	end
+    local some_var = ...
+    local name = some_var:match( "^.*%[(.*)%].* has come online")
+    if name then
+        for char_name, class in pairs(name_table) do
+            if name == char_name then
+                if name == "Dandelo" then
+                    local msg = "False alarm, telch's main is still " .. class
+                    SendChatMessage(msg, "GUILD")
+                    break
+                else
+                    local msg = "Ohay, telch is on his " .. class .. " how's the main switch going?"
+                    SendChatMessage(msg, "GUILD")
+                end
+            end
+        end
+    end
 end)
 
-	--elseif some_var:sub(-8) == "offline." then
-	--	print("SOMEONE LOGGED OFF")
+    --elseif some_var:sub(-8) == "offline." then
+    --  print("SOMEONE LOGGED OFF")
 
